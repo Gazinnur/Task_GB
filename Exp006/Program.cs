198fca11af082ad3925803732af749eda149c1ca
gazinnur <gazinnur@mail.ru>
2022-11-06 23:45:54 +0300

    Решение задачи 006

---
+++ Exp006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int Num=Convert.ToInt32(Console.Read());
+if(Num%2==0) Console.WriteLine("число четное");
+else Console.WriteLine("число не четное");
