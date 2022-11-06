87ddd6cda1fd986c09902ea431f7ca03d6ff2cf1
gazinnur <gazinnur@mail.ru>
2022-11-07 00:14:37 +0300

    Решение задачи 010

---
+++ Exp010/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехначное число");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num < 0) Num = -Num;
+if (Num > 99 && Num <= 999)
+{
+    int Third = Num/10 % 10;
+    Console.WriteLine(Third);
+}
+else Console.WriteLine("Вы ввели не трехначное число");
