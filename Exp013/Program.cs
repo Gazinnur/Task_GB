f72bf558cdf7c7861ef91180939c21882900d0c6
gazinnur <gazinnur@mail.ru>
2022-11-07 01:03:15 +0300

    Решение задачи 013

---
+++ Exp013/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите первое число");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1%Num2==0)
+{
+    Console.WriteLine($"Первое число кратно второму");
+}
+else 
+{
+    double ost=Num1%Num2;
+    Console.WriteLine($"остаток деления первого числа на второе равен {ost}/{Num2}");
+}
+Console.WriteLine();
