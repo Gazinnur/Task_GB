8182471f067f863ab895b05cda73d8143e84b91d
gazinnur <gazinnur@mail.ru>
2022-11-06 23:41:35 +0300

    Решение задачи 005

---
+++ Exp005/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+Console.WriteLine("y=sin(x)");
+Console.WriteLine("Введите х");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Math.Sin(x);
+Console.WriteLine($"в уравнении y=sin(x) при х = {x} y = {y}");
