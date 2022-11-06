bf9cbc3859835acb9db2db8e1ef5d6f4056eb4be
gazinnur <gazinnur@mail.ru>
2022-11-06 23:28:57 +0300

    Решение задачи 004

---
+++ Exp004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти максимальное из трех чисел
+Console.WriteLine("Введите 3 числа");
+int Num1=Convert.ToInt32(Console.ReadLine());
+int Num2=Convert.ToInt32(Console.ReadLine());
+int Num3=Convert.ToInt32(Console.ReadLine());
+int max=Num1;
+
+if (Num2>max)
+{
+    max=Num2;
+}
+if(Num3>max) max=Num3;
+Console.WriteLine($"Max = {max}");
