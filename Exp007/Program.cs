ef43a774476bd354c4c17b20e69451c4625d6f06
gazinnur <gazinnur@mail.ru>
2022-11-06 23:54:51 +0300

    Решение задачи 007

---
+++ Exp007/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать числа от -N до N
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0) N = -N;
+for (int i = -N; i <= N; i++)
+{
+    Console.Write($"{i} ");
+}
