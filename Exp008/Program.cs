2d183560ec156e77cff6496d50ffae937b8f6b0d
gazinnur <gazinnur@mail.ru>
2022-11-07 00:01:01 +0300

    Решение задачи 008

---
+++ Exp008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N<0) N=-N;
+for (int i = 1; i <=N; i++)
+{
+    if (i%2==0)
+    {
+        Console.Write($"{i} ");
+    }
+}
