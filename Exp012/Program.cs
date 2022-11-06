8f3ca4fe912bb0748ddf00e96c9e79dd1af9b695
gazinnur <gazinnur@mail.ru>
2022-11-07 00:51:52 +0300

    Решение задачи 012

---
+++ Exp012/Program.cs
@@ -0,0 +1,21 @@
+﻿//Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехначное число");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num > 99 && Num <= 999)
+{
+    int Third = Num % 10;
+    int first = Num /100;
+    Console.WriteLine($"{first}{Third}");
+}
+else if (Num < -99 && Num >=-999)
+{
+    int Third = -Num % 10;
+    int first = Num /100;
+    Console.WriteLine($"{first}{Third}");
+}
+else Console.WriteLine("Вы ввели не трехначное число");
+
+
+Console.WriteLine();
