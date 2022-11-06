c08befce041f778cc4b84e4c45e29592ac78cc9e
gazinnur <gazinnur@mail.ru>
2022-11-07 01:30:04 +0300

    Поправил задание 003

--- 0-14/Exp003/Program.cs
+++ 0-14/Exp003/Program.cs
@@ -1,15 +1,12 @@
 ﻿// По заданному номеру дня недели вывести его название
 Console.WriteLine("Введите номер деня недали");
 int Num = Convert.ToInt32(Console.ReadLine());
-if (Num>7||Num<1)
-{
-    Console.WriteLine("В недели 7 дней");
-    
-}
-else 
-{if (Num<6)
-    {
-        Console.WriteLine("Это будний день");
-    }
-    else Console.WriteLine("Это выходной");
-}
+if (Num>7||Num<1) Console.WriteLine("В недели 7 дней");
+if (Num==1)Console.WriteLine("Это Понедельник"); 
+if (Num==2)Console.WriteLine("Это Вторник"); 
+if (Num==3)Console.WriteLine("Это Среда"); 
+if (Num==4)Console.WriteLine("Это Четверг"); 
+if (Num==5)Console.WriteLine("Это Пятница"); 
+if (Num==6)Console.WriteLine("Это Суббота"); 
+if (Num==7)Console.WriteLine("Это Воскресение"); 
+Console.WriteLine();
---
+++ Exp003/Program.cs
@@ -0,0 +1,15 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер деня недали");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num>7||Num<1)
+{
+    Console.WriteLine("В недели 7 дней");
+    
+}
+else 
+{if (Num<6)
+    {
+        Console.WriteLine("Это будний день");
+    }
+    else Console.WriteLine("Это выходной");
+}
