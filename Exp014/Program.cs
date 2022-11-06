2e99259876089f56fc78118a3686b3acd157b40e
gazinnur <gazinnur@mail.ru>
2022-11-07 01:21:03 +0300

    Решение задачи 014

---
+++ Exp014/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num < 0)Num=-Num;
+if (Num < 99 && Num > -99) Console.WriteLine("третьего числа нет");
+else {if (Num > 1000)
+{
+    Num=Num/10;
+}
+Console.WriteLine($"{Num%10}");}
+
+Console.WriteLine();
